ee2e380fde1d27f123f8c418a1df29da441889b7
Joey Lamanna <jlamanna@villanova.edu>
2024-04-04 12:33:22 -0400

    Added a comment component with a scroll bar

---
+++ client/src/pages/CommentSection.jsx
@@ -0,0 +1,42 @@
+// components/CommentSection.js
+import React from 'react';
+import '/Users/joeylamanna/Desktop/csc4700-team-project-socialconnect/client/src/Styles/Dashboard.css';
+
+const CommentSection = () => {
+    // Sample list of comments with user, platform, and comment information
+    const comments = [
+      { user: 'User1', platform: 'Twitter', comment: 'This is the first comment.' },
+      { user: 'User2', platform: 'Facebook', comment: 'This is the second comment.' },
+      { user: 'User3', platform: 'Instagram', comment: 'This is the third comment.' },
+      { user: 'User1', platform: 'Twitter', comment: 'This is the first comment.' },
+      { user: 'User2', platform: 'Facebook', comment: 'This is the second comment.' },
+      { user: 'User3', platform: 'Instagram', comment: 'This is the third comment.' },
+      { user: 'User1', platform: 'Twitter', comment: 'This is the first comment.' },
+      { user: 'User2', platform: 'Facebook', comment: 'This is the second comment.' },
+      { user: 'User3', platform: 'Instagram', comment: 'This is the third comment.' },
+      { user: 'User1', platform: 'Twitter', comment: 'This is the first comment.' },
+      { user: 'User2', platform: 'Facebook', comment: 'This is the second comment.' },
+      { user: 'User3', platform: 'Instagram', comment: 'This is the third comment.' },
+      { user: 'User1', platform: 'Twitter', comment: 'This is the first comment.' },
+      { user: 'User2', platform: 'Facebook', comment: 'This is the second comment.' },
+      { user: 'User3', platform: 'Instagram', comment: 'This is the third comment.' },
+      // Add more comments as needed
+    ];
+  
+    return (
+      <div className="comment-section-container">
+        <div className="comment-section">
+          <h2 className="comment-header">Comments</h2>
+          <ul className="comment-list">
+            {comments.map((comment, index) => (
+              <li key={index} className="comment-item">
+                <strong>{comment.user}</strong> from {comment.platform}: {comment.comment}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  
+  export default CommentSection;
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import '/Users/joeylamanna/Desktop/csc4700-team-project-socialconnect/client/src/Styles/Dashboard.css';
 import Sidebar from '/Users/joeylamanna/Desktop/csc4700-team-project-socialconnect/client/src/pages/Sidebar.jsx'
+import CommentSection from './CommentSection';
 
 const home = () => {
   return (
     <div className="dashboard">
       <Sidebar/>
-      <div className="box">Box 1</div>
+      <div className="box">Box 1 </div>
       <div className="box">Box 2</div>
-      <div className="box">Box 3</div>
+      <div className="comment"> <CommentSection /></div>
       <div className="box">Box 4</div>
     </div>
   );
