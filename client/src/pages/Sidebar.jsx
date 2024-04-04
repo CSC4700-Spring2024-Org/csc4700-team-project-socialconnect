ba4b2dafbe9c624ad68dc5aa34b4661c5a47dc61
Joey Lamanna <jlamanna@villanova.edu>
2024-04-03 23:04:42 -0400

    Added a sidebar with working links

---
+++ client/src/pages/Sidebar.jsx
@@ -0,0 +1,28 @@
+// components/Sidebar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '/Users/joeylamanna/Desktop/csc4700-team-project-socialconnect/client/src/Styles/Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/analytics">Analytics</Link>
+        </li>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="/calendar">Calendar</Link>
+        </li>
+        {/* Add more links for other pages */}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '/Users/joeylamanna/Desktop/csc4700-team-project-socialconnect/client/src/Styles/Dashboard.css';
+import Sidebar from '/Users/joeylamanna/Desktop/csc4700-team-project-socialconnect/client/src/pages/Sidebar.jsx'
 
 const home = () => {
   return (
     <div className="dashboard">
+      <Sidebar/>
       <div className="box">Box 1</div>
       <div className="box">Box 2</div>
       <div className="box">Box 3</div>
