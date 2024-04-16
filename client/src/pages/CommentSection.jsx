0ce6529eca4d9a4845811faab7cf5739f1dd42e4
chume2 <charliehume0806@gmail.com>
2024-04-15 23:05:07 -0400

    Deleted unwanted UI elements

--- client/src/pages/CommentSection.jsx
+++ client/src/pages/CommentSection.jsx
@@ -28,9 +28,9 @@ const CommentSection = () => {
       <section class="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
         <div class="max-w-2xl mx-auto px-4">
           <div class="flex justify-between items-center mb-6">
-            <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Discussion (20)</h2>
+            <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Comments (20)</h2>
           </div>
-          <form class="mb-6">
+          {/* <form class="mb-6">
           <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
             <label for="comment" class="sr-only">Your comment</label>
             <textarea id="comment" rows="6"
@@ -41,7 +41,7 @@ const CommentSection = () => {
             class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
             Post comment
         </button>
-        </form>
+        </form> */}
     <article class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
         <footer class="flex justify-between items-center mb-2">
             <div class="flex items-center">
