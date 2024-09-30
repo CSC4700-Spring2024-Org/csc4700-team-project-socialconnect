5da138f97d7d3c90583d4609ea866df72ac6b232
chume2 <charliehume0806@gmail.com>
2024-09-30 18:27:30 -0400

    started implementing Terms and Conditions

--- client/src/pages/TermsAndConditions.jsx
+++ client/src/pages/TermsAndConditions.jsx
@@ -2,8 +2,73 @@ import React from 'react'
 
 const TermsAndConditions = () => {
   return (
-    <div>Terms and Conditions Page</div>
-  )
+    <>
+      <div className='terms-container'>
+        <div className='terms-content'>
+          <h1>Terms and Conditions</h1>
+
+          <h2>Agreement between User and https://www.danbfrost.com </h2>
+          <p>Please keep in mind that if you directly disclose personally identifiable information or personally sensitive data through the Company's public message boards, this information may be collected and used by others.</p>
+          <p>We do not collect any personal information about you unless you voluntarily provide it to us. However, you may be required to provide certain personal information to us when you elect to use certain products or services. These may include:</p>
+          <ul>
+            <li>Registering for an account</li>
+            <li>Entering a sweepstakes or contest sponsored by us or one of our partners</li>
+            <li>Signing up for special offers from selected third parties</li>
+            <li>Sending us an email message</li>
+            <li>Submitting your credit card or other payment information when ordering and purchasing products and services</li>
+          </ul>
+          <p>We also may gather additional personal or non-personal information in the future.</p>
+
+          <h2>Sharing Information with Third Parties</h2>
+          <p>The Company does not sell, rent, or lease its customer lists to third parties.</p>
+          <p>The Company may share data with trusted partners to help perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services to the Company, and they are required to maintain the confidentiality of your information.</p>
+          <p>The Company may disclose your personal information, without notice, if required to do so by law or in the good faith belief that such action is necessary to:</p>
+          <ul>
+            <li>Conform to the edicts of the law or comply with legal process served on the Company or the site</li>
+            <li>Protect and defend the rights or property of the Company</li>
+            <li>Act under exigent circumstances to protect the personal safety of users of the Company or the public</li>
+          </ul>
+
+          <h2>Automatically Collected Information</h2>
+          <p>The Company may automatically collect information about your computer hardware and software. This information can include your IP address, browser type, domain names, access times, and referring website addresses. This information is used for the operation of the service, to maintain quality of the service, and to provide general statistics regarding the use of the Company's website.</p>
+
+          <h2>Links</h2>
+          <p>This website contains links to other sites. Please be aware that we are not responsible for the content or privacy practices of such other sites. We encourage our users to be aware when they leave our site and to read the privacy statements of any other site that collects personally identifiable information.</p>
+
+          <h2>Right to Deletion</h2>
+          <p>Subject to certain exceptions set out below, on receipt of a verifiable request from you, we will:</p>
+          <ul>
+            <li>Delete your personal information from our records</li>
+            <li>Direct any service providers to delete your personal information from their records</li>
+          </ul>
+          <p>Please note that we may not be able to comply with requests to delete your personal information if it is necessary to:</p>
+          <ul>
+            <li>Complete the transaction for which the personal information was collected</li>
+            <li>Detect security incidents, protect against malicious, deceptive, fraudulent, or illegal activity; or prosecute those responsible for that activity</li>
+            <li>Debug to identify and repair errors that impair existing intended functionality</li>
+            <li>Exercise free speech, ensure the right of another consumer to exercise their right of free speech</li>
+            <li>Comply with an existing legal obligation</li>
+            <li>Otherwise use your personal information, internally, in a lawful manner that is compatible with the context in which you provided the information</li>
+          </ul>
+
+          <h2>Children Under Thirteen</h2>
+          <p>The Company does not knowingly collect personally identifiable information from children under the age of 13. If you are under the age of 13, you must ask your parent or guardian for permission to use this website.</p>
+
+          <h2>Disconnecting your Company's Account from Third Party Websites</h2>
+          <p>You will be able to connect your Company's account to third-party accounts. By connecting your Company account to your third-party account, you acknowledge and agree to the continuous release of information about you to others in accordance with your privacy settings on those third-party sites. If you do not want information about you to be shared in this manner, do not use this feature. You may disconnect your account from a third-party account at any time.</p>
+
+          <h2>Email Communications</h2>
+          <p>From time to time, the Company may contact you via email for the purpose of providing announcements, promotional offers, alerts, confirmations, surveys, and/or other general communication.</p>
+
+          <h2>Changes to This Statement</h2>
+          <p>The Company reserves the right to change this Policy from time to time. When changes to this Policy are significant, we will inform you by sending a notice via email or placing a prominent notice on our website.</p>
+          <footer className='terms-footer'>
+          Effective as of September 30, 2024    |    Contact: socialconnectbiznuz@gmail.com
+        </footer>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default TermsAndConditions
