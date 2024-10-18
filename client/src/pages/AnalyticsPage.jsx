1638f5651aa4f062512149f77ce53dccc2043a44
chume2 <charliehume0806@gmail.com>
2024-10-18 02:10:02 -0400

    got rid of old, commented out code

--- client/src/pages/AnalyticsPage.jsx
+++ client/src/pages/AnalyticsPage.jsx
@@ -1,115 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { AgCharts } from 'ag-charts-react';
-// import { useSelector } from 'react-redux';
-// import NoAccount from '../components/NoAccount';
-// import Spinner from '../components/Spinner';
-// import { FaInstagram, FaTiktok, FaYoutube } from 'react-icons/fa';
-
-// export default function AnalyticsPage() {
-//   const { instaPage, isLoadingInsta } = useSelector((state) => state.insta);
-//   const { user, isLoading } = useSelector((state) => state.auth);
-
-//   const [instaChartOptions, setInstaChartOptions] = useState({
-//     background: {
-//       fill: 'transparent'
-//     },
-//     axes: [
-//       {
-//         type: 'time',
-//         position: 'bottom',
-//         label: {
-//           format: '%b'
-//         }
-//       },
-//       {
-//         type: 'number',
-//         position: 'left',
-//         title: { 
-//           text: 'Likes',
-//           color: 'black'
-//         }, 
-//       }
-//     ],
-//     data: [], 
-//     series: [
-//       {
-//         type: 'line',
-//         xKey: 'date',
-//         yKey: 'Instagram',
-//         stroke: '#FF69B4',
-//         marker: {
-//           enabled: true,
-//           size: 7,
-//           fill: '#FF69B4'
-//         },
-//         tooltip: {
-//           enabled: true,
-//           renderer: (params) => {
-//             const formattedDate = new Date(params.datum.date).toLocaleDateString('en-US', {
-//               year: 'numeric',
-//               month: 'short',
-//               day: 'numeric'
-//             });
-//             return {
-//               title: formattedDate,
-//               content: `${params.datum.Instagram} likes`,
-//               backgroundColor: '#FF69B4'
-//             };
-//           }
-//         }
-//       }
-//     ],
-//     title: {
-//       text: 'Instagram Likes',
-//       fontSize: 18,
-//       fontFamily: 'Futura',
-//       color: 'black',
-//       style: 'bold'
-//     }
-//   });
-
-//   const [selectedOption, setSelectedOption] = useState('Instagram'); 
-
-//   useEffect(() => {
-//     if (!isLoadingInsta && instaPage) {
-//       const likeCounts = instaPage.business_discovery.media.data.map(media => media.like_count);
-//       const dates = instaPage.business_discovery.media.data.map(media => media.timestamp);
-
-//       const dynamicData = likeCounts.map((likeCount, index) => {
-//         const dateObject = new Date(dates[index]);
-//         return {
-//           Instagram: likeCount,
-//           date: dateObject,
-//           TikTok: 0, 
-//           Youtube: 0 
-//         };
-//       });
-
-//       setInstaChartOptions((prevOptions) => ({
-//         ...prevOptions,
-//         data: dynamicData
-//       }));
-//     }
-//   }, [isLoadingInsta, instaPage]); 
-
-//   if (!isLoading && (user && !user.instaRefresh)) {
-//     return <NoAccount />;
-//   }
-//   if (isLoadingInsta || !instaPage) {
-//     return <Spinner />;
-//   }
-
-//   const handlePlatformChange = (platform) => {
-//     setSelectedOption(platform);
-//     console.log(`Selected option: ${platform}`);
-//   };
-
-//   return (
-//   );
-// }
-
-
-
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import NoAccount from '../components/NoAccount';
@@ -251,14 +139,9 @@ const AnalyticsPage = () => {
             <AgCharts options={instaChartOptions} />
       </div>
 
-      <div className="demographics">
+      {/* <div className="demographics">
         <h3>Demographics</h3>
-        <ul>
-          <li>Age: 18-24</li>
-          <li>Gender: Female</li>
-          <li>Location: Los Angeles</li>
-        </ul>
-      </div>
+      </div> */}
     </div>
   );
 };
